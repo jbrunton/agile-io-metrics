d912311365d508ef2d42141b65d79cb8eb360ab0
John Brunton <jbrunton@zipcar.com>
2015-12-20 19:31:51 +0000

    fix failing spec

--- spec/views/organizations/show.html.erb_spec.rb
+++ spec/views/organizations/show.html.erb_spec.rb
@@ -5,10 +5,12 @@ RSpec.describe "organizations/show", type: :view do
     @organization = assign(:organization, Organization.create!(
       :name => "Name"
     ))
+    @admins = assign(:admins, [create(:user, email: 'email@example.com')])
   end
 
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(/Name/)
+    expect(rendered).to match(/email@example.com/)
   end
 end
