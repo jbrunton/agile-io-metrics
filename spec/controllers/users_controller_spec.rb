547f5943533868b51ac03c7e9428691fe02dbec5
John Brunton <jbrunton@zipcar.com>
2015-12-20 17:52:05 +0000

    remove user actions that are handled by devise

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -20,108 +20,16 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # User. As you add validations to User, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    attributes_for(:user)
-  }
-
-  let(:invalid_attributes) {
-    { email: '' }
-  }
-
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # UsersController. Be sure to keep this updated too.
   let(:valid_session) { {} }
-
-  describe "GET #index" do
-    it "assigns all users as @users" do
-      user = create(:user)
-      get :index, {}, valid_session
-      expect(assigns(:users)).to eq([user])
-    end
-  end
 
   describe "GET #show" do
     it "assigns the requested user as @user" do
       user = create(:user)
       get :show, {:id => user.to_param}, valid_session
       expect(assigns(:user)).to eq(user)
-    end
-  end
-
-  describe "GET #new" do
-    it "assigns a new user as @user" do
-      get :new, {}, valid_session
-      expect(assigns(:user)).to be_a_new(User)
-    end
-  end
-
-  describe "GET #edit" do
-    it "assigns the requested user as @user" do
-      user = create(:user)
-      get :edit, {:id => user.to_param}, valid_session
-      expect(assigns(:user)).to eq(user)
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        attributes_for(:user)
-      }
-
-      it "updates the requested user" do
-        user = create(:user)
-
-        put :update, {:id => user.to_param, :user => new_attributes}, valid_session
-
-        user.reload
-        expect(user.email).to eq(new_attributes[:email])
-      end
-
-      it "assigns the requested user as @user" do
-        user = User.create! valid_attributes
-        put :update, {:id => user.to_param, :user => valid_attributes}, valid_session
-        expect(assigns(:user)).to eq(user)
-      end
-
-      it "redirects to the user" do
-        user = User.create! valid_attributes
-        put :update, {:id => user.to_param, :user => valid_attributes}, valid_session
-        expect(response).to redirect_to(user)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns the user as @user" do
-        user = create(:user)
-        put :update, {:id => user.to_param, :user => invalid_attributes}, valid_session
-        expect(assigns(:user)).to eq(user)
-      end
-
-      it "re-renders the 'edit' template" do
-        user = create(:user)
-        put :update, {:id => user.to_param, :user => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested user" do
-      user = User.create! valid_attributes
-      expect {
-        delete :destroy, {:id => user.to_param}, valid_session
-      }.to change(User, :count).by(-1)
-    end
-
-    it "redirects to the users list" do
-      user = User.create! valid_attributes
-      delete :destroy, {:id => user.to_param}, valid_session
-      expect(response).to redirect_to(users_url)
     end
   end
 
