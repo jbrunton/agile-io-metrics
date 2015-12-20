535bc53eba84f041690c9d3115c5f4c165925040
John Brunton <jbrunton@zipcar.com>
2015-12-20 22:40:35 +0000

    add scaffolding for teams

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151220182031) do
+ActiveRecord::Schema.define(version: 20151220223946) do
 
   create_table "organizations", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20151220182031) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], name: "index_roles_on_name"
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "organization_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "teams", ["organization_id"], name: "index_teams_on_organization_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
