c3d2cf96590492fe0ba13af6c398000fdc3f4211
John Brunton <jbrunton@zipcar.com>
2015-12-20 16:41:40 +0000

    remove email validation already done by devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-      uniqueness:  { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "requires an email" do
-    user = build(:user, email: '')
-    expect(user).not_to be_valid
-  end
 
-  it "requires a valid email" do
-    user = build(:user, email: 'not.valid')
-    expect(user).not_to be_valid
-  end
-
-  it "requires a unique email" do
-    create(:user, email: 'example@example.com')
-    user = build(:user, email: 'example@example.com')
-    expect(user).not_to be_valid
-  end
-
-  it "validates the email successsfully" do
-    user = build(:user, email: 'example@example.com')
-    expect(user).to be_valid
-  end
 end
