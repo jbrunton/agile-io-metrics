8749c65a2f56ab0cc5c54e1c1f5036a9eae35be8
John Brunton <jbrunton@zipcar.com>
2015-12-20 14:22:23 +0000

    add scaffolding for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219095359) do
+ActiveRecord::Schema.define(version: 20151220142153) do
 
   create_table "organizations", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
