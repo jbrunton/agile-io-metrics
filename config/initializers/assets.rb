2f2ff12398c74404466290e7d7e57c2b9012f3ba
John Brunton <jbrunton@zipcar.com>
2015-12-20 12:48:11 +0000

    fix asset whitelist

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile << /materialize\/font\/.+\.(?:svg|eot|woff|woff2|ttf)\z/
+# Rails.application.config.assets.precompile << /materialize\/font\/.+\.(?:svg|eot|woff|woff2|ttf)\z/
 
-NonStupidDigestAssets.whitelist << 'materialize/font/**/*'
+NonStupidDigestAssets.whitelist << /materialize\/font\/.+\.(?:svg|eot|woff|woff2|ttf)\z/
