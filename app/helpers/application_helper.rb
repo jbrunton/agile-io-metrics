857906218ba6481d47346a43a598cf818823dee4
John Brunton <jbrunton@zipcar.com>
2015-12-20 00:40:44 +0000

    use materialize

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def fab_link_to(icon, path, options={})
     options = options.clone
-    options[:class] = (options[:class] || '') + ' fab'
+    options[:class] = (options[:class] || '') + ' fab waves-effect waves-light'
     text = "<i class='material-icons'>#{icon}</i>".html_safe
     link_to(text, path, options)
   end
