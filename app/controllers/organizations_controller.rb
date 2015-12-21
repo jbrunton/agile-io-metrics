07861c353e45e804ea1fd0364f14a191b2e41aee
John Brunton <jbrunton@zipcar.com>
2015-12-21 09:18:46 +0000

    add blank layout for organizations#show action

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,6 @@
 class OrganizationsController < ApplicationController
+  layout 'blank', only: [:show]
+
   before_action :set_organization, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
   # after_action :verify_authorized
