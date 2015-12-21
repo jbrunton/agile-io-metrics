33130d1d417637b9b4de4a5483c868b081768818
John Brunton <jbrunton@zipcar.com>
2015-12-21 12:11:19 +0000

    fix failing spec

--- spec/views/teams/new.html.erb_spec.rb
+++ spec/views/teams/new.html.erb_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "teams/new", type: :view do
   before(:each) do
-    assign(:team, Team.new(
+    @organization = assign(:organization, create(:organization))
+    @team = assign(:team, Team.new(
       :name => "MyString",
-      :organization => nil
+      :organization => @organization
     ))
+    assign(:record, [@organization, @team])
   end
 
   it "renders new team form" do
     render
 
-    assert_select "form[action=?][method=?]", teams_path, "post" do
+    assert_select "form[action=?][method=?]", organization_teams_path(@organization), "post" do
 
       assert_select "input#team_name[name=?]", "team[name]"
-
-      assert_select "input#team_organization_id[name=?]", "team[organization_id]"
     end
   end
 end
