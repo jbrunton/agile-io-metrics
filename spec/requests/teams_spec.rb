c5fce68e1f20672cb7adbfefd1b013567244c8a0
John Brunton <jbrunton@zipcar.com>
2015-12-21 12:07:09 +0000

    fix failing spec

--- spec/requests/teams_spec.rb
+++ spec/requests/teams_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Teams", type: :request do
   describe "GET /teams" do
     it "works! (now write some real specs)" do
-      get teams_path
+      get organization_teams_path(create(:organization))
       expect(response).to have_http_status(200)
     end
   end
