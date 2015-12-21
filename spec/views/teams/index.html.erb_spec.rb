bec6fb5621e562aeaa8c6f0a6591d80e602caf80
John Brunton <jbrunton@zipcar.com>
2015-12-21 12:09:37 +0000

    fix failing spec

--- spec/views/teams/index.html.erb_spec.rb
+++ spec/views/teams/index.html.erb_spec.rb
@@ -2,21 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "teams/index", type: :view do
   before(:each) do
-    assign(:teams, [
-      Team.create!(
-        :name => "Name",
-        :organization => nil
-      ),
-      Team.create!(
-        :name => "Name",
-        :organization => nil
-      )
+    organization = assign(:organization, create(:organization))
+    @teams = assign(:teams, [
+        create(:team, organization: organization),
+        create(:team, organization: organization)
     ])
   end
 
   it "renders a list of teams" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => @teams[0].name
+    assert_select "tr>td", :text => @teams[1].name
   end
 end
