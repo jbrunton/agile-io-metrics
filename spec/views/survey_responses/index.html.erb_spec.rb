4f1c0ae262111caf58f1ac206fed6a87a73f557e
John Brunton <jbrunton@zipcar.com>
2015-12-23 23:00:44 +0000

    list user emails on responses page

--- spec/views/survey_responses/index.html.erb_spec.rb
+++ spec/views/survey_responses/index.html.erb_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "survey_responses/index", type: :view do
+  let(:survey_instance) { create(:survey_instance) }
+  let(:survey_responses) {[
+      create(:survey_response, survey_instance: survey_instance),
+      create(:survey_response, survey_instance: survey_instance)
+  ]}
+
   before(:each) do
-    assign(:survey_responses, [
-      SurveyResponse.create!(
-        :user => nil,
-        :survey_instance => nil
-      ),
-      SurveyResponse.create!(
-        :user => nil,
-        :survey_instance => nil
-      )
-    ])
+    assign(:survey_responses, survey_responses)
   end
 
   it "renders a list of survey_responses" do
     render
-    assert_select "tr>td", :text => nil.to_s, :count => 4
+
+    survey_responses.each do |survey_response|
+      assert_select "tr>td", :text => survey_response.user.email
+    end
   end
 end
