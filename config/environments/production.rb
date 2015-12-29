f30ec7a63723aa60a54087c7bcbca46a5237c6f8
John Brunton <jbrunton@zipcar.com>
2015-12-29 12:01:50 +0000

    specify default host on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
     :domain         => 'metrics.agileplanning.io', #eg: 'yourappname.herokuapp.com'
     :authentication => :plain,
   }
+  config.action_mailer.default_url_options = { host: 'metrics.agileplanning.io' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
